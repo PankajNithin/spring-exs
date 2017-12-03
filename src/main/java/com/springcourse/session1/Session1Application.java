5807c597d65a3c2e1ba9bb682771d24d93d71442
pankajNithin <nithinpankaj@gmail.com>
2017-12-03 22:03:30 +0530

    Initial commit

---
+++ src/main/java/com/springcourse/session1/Session1Application.java
@@ -0,0 +1,12 @@
+package com.springcourse.session1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Session1Application.class, args);
+	}
+}
